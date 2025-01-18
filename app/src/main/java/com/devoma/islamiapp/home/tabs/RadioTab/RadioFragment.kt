f84d77332ea5cbca7d0a92e34ead0ca7f2ad40e0
1mohamed0 <mohamedmahdy.info@gmail.com>
2025-01-18 20:47:32 +0200

    + Add Sebha Tab + Bug Fix

--- app/src/main/java/com/devoma/islamiapp/home/tabs/RadioTab/RadioFragment.kt
+++ app/src/main/java/com/devoma/islamiapp/home/tabs/RadioTab/RadioFragment.kt
@@ -1,19 +1,24 @@
 package com.devoma.islamiapp.home.tabs.RadioTab
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.devoma.islamiapp.R
-
+import androidx.fragment.app.Fragment
+import com.devoma.islamiapp.databinding.FragmentRadioBinding
 
 class RadioFragment : Fragment() {
+
+    lateinit var viewBinding: FragmentRadioBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_radio, container, false)
+        viewBinding = FragmentRadioBinding.inflate(inflater, container, false)
+        return viewBinding.root
     }
 }
+
+
+
--- app/src/main/java/com/devoma/islamiapp/home/tabs/SebhaTab/SebhaFragment.kt
+++ app/src/main/java/com/devoma/islamiapp/home/tabs/SebhaTab/SebhaFragment.kt
@@ -1,19 +1,54 @@
 package com.devoma.islamiapp.home.tabs.SebhaTab
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.devoma.islamiapp.R
+import com.devoma.islamiapp.databinding.FragmentSebhaBinding
 
 
 class SebhaFragment : Fragment() {
+    private var currentRotation = 0f
+    var courrantZekrIndex = 0
+    var count = 0
+    lateinit var zekrList: MutableList<String>
+    lateinit var viewBinding: FragmentSebhaBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sebha, container, false)
+        viewBinding = FragmentSebhaBinding.inflate(inflater,container,false)
+        return viewBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        zekrList = resources.getStringArray(R.array.azkarName).toMutableList()
+        viewBinding.zekrName.text = zekrList[courrantZekrIndex]
+        viewBinding.zekrCount.text = "$count"
+        viewBinding.sebhaImv.setOnClickListener {
+            rotsteImage()
+            if (count < 33) {
+                count++
+            } else {
+                count = 0
+                courrantZekrIndex =
+                    if (courrantZekrIndex < zekrList.size - 1) ++courrantZekrIndex else 0
+                viewBinding.zekrName.text = zekrList[courrantZekrIndex]
+            }
+            viewBinding.zekrCount.text = "$count"
+        }
+    }
+
+    private fun rotsteImage() {
+        currentRotation += 10f
+        viewBinding.sebhaImv.rotation = currentRotation
+    }
+
+    private fun updataZekr() {
+        viewBinding.zekrName.text = zekrList[courrantZekrIndex]
     }
 }
